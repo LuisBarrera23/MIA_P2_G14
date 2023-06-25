b4a185c1bdbfbd748090a58b483257fa3eeafd12
RMorales202010033 <82615559+RMorales202010033@users.noreply.github.com>
2023-06-24 21:24:36 -0600

    Transfer bucket-server

--- Commands/Transfer.py
+++ Commands/Transfer.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 from Singleton import Singleton
+import boto3
 
 class Transfer():
     def __init__(self, pfrom, pto, typeto, typefrom) -> None:
@@ -13,6 +14,12 @@ class Transfer():
     def run(self):
         if self.typefrom == "server" and self.typeto=="server":
             self.Local()
+        elif self.typefrom == "server" and self.typeto=="bucket":
+            self.Server_bucket()
+        elif self.typefrom == "bucket" and self.typeto=="server":
+            self.Bucket_server()
+        elif self.typefrom == "bucket" and self.typeto=="bucket":
+            self.Cloud()
         
     def Local(self):
         ruta_directorio = os.path.join('Archivos', self.pfrom)
@@ -31,28 +38,6 @@ class Transfer():
         # Validar la existencia o creación del destino (ruta_destino)
         if not os.path.exists(ruta_destino):
             os.makedirs(ruta_destino)
-
-        # Validar si ruta_directorio es una carpeta
-        # if os.path.isdir(ruta_directorio):
-        #     # Transferir el contenido de la carpeta raíz
-        #     for root, dirs, files in os.walk(ruta_directorio):
-        #         for dir in dirs:
-        #             source_dir = os.path.join(root, dir)
-        #             destination_dir = os.path.join(ruta_destino, os.path.relpath(source_dir, ruta_directorio))
-        #             if not os.path.exists(destination_dir):
-        #                 os.makedirs(destination_dir)
-
-        #         for file in files:
-        #             source_file = os.path.join(root, file)
-        #             destination_file = os.path.join(ruta_destino, os.path.relpath(source_file, ruta_directorio))
-        #             shutil.move(source_file, destination_file)
-                    
-        #     # Eliminar subdirectorios vacíos en la carpeta de origen
-        #     for root, dirs, files in os.walk(ruta_directorio, topdown=False):
-        #         for dir in dirs:
-        #             dir_path = os.path.join(root, dir)
-        #             if len(os.listdir(dir_path)) == 0:
-        #                 os.rmdir(dir_path)
         
         if os.path.isdir(ruta_directorio):
             if os.path.exists(ruta_destino) and os.path.isdir(ruta_destino):
@@ -78,11 +63,6 @@ class Transfer():
                                 contador += 1
                                 current_folder[-1] = nombre_copia
 
-                            # if not os.path.exists(str(destination_dir)):
-                            #     os.makedirs(destination_dir)
-                                # os.makedirs(destination_dir, exist_ok=True)
-                            # print(source_dir)
-                            # print(destination_dir)
                             shutil.move(source_dir, destination_dir)                            
 
                         for file in files:
@@ -124,6 +104,146 @@ class Transfer():
         print("Transferencia completada.")
          
         self.instancia.consola += "Transferencia completada.\n"
+    
+    def Server_bucket(self):
+        pass
+    
+    def Bucket_server(self):
+        session = boto3.Session(
+            aws_access_key_id=self.instancia.accesskey,
+            aws_secret_access_key=self.instancia.secretaccesskey,
+        )
+        carpeta = 'Archivos'
+        s3 = session.client('s3')
+        bucket_name = 'proyecto2g14'
+        
+        ruta_directorio = carpeta + "/" + self.pfrom
+        ruta_destino = carpeta + "/" + self.pto
+        
+        if ruta_directorio.endswith(".txt"):
+            # Separar la ruta del nombre de archivo
+            carpeta_s3, archivo = os.path.split(ruta_directorio)
+
+            try:
+                # Verificar si el archivo existe en S3
+                s3.head_object(Bucket=bucket_name, Key=ruta_directorio)
+            except s3.exceptions.ClientError as e:
+                # Si ocurre un error, el archivo no existe
+                if e.response['Error']['Code'] == '404':
+                    print(f"El archivo '{archivo}' no existe en S3.")
+                    self.instancia.consola += f"El archivo '{archivo}' no existe en S3.\n"
+                else:
+                    print(f"Ocurrió un error al verificar el archivo: {e}")
+                    self.instancia.consola += f"Ocurrió un error al verificar el archivo: {e}\n"
+                return
+
+            if not os.path.exists(ruta_destino):
+                try:
+                    os.makedirs(ruta_destino)
+                    # print(f"Se ha creado la ruta de destino '{ruta_destino}'.")
+                    # self.instancia.consola += f"Se ha creado la ruta de destino '{ruta_destino}'.\n"
+                except OSError as e:
+                    print(f"Error al crear la ruta de destino '{ruta_destino}': {e}")
+                    self.instancia.consola += f"Error al crear la ruta de destino '{ruta_destino}': {e}\n"
+                    return
+
+            # Copiar el archivo a la ruta de destino en tu máquina física
+            try:
+                ruta_destino_archivo = ruta_destino + '/' + archivo
+                archivo_dividido = archivo.split(".")
+                nombre_sin_extension = archivo_dividido[0]
+                extension = archivo_dividido[1]
+                # Verificar si el archivo ya existe en la ruta de destino
+                contador = 1
+                while os.path.exists(ruta_destino_archivo):
+                    nombre_copia = f"{nombre_sin_extension}_copia{contador}.{extension}"
+                    ruta_destino_archivo = ruta_destino + "/" + nombre_copia
+                    contador += 1
+                # s3.download_file(bucket_name, ruta_directorio, ruta_destino_archivo)
+                # print(f"El archivo '{archivo}' se ha copiado correctamente.")
+                # self.instancia.consola += f"El archivo '{archivo}' se ha copiado correctamente.\n"
+                s3_resource = session.resource('s3')
+                bucket = s3_resource.Bucket(bucket_name)
+                s3_resource.Object(bucket_name, ruta_directorio).download_file(ruta_destino_archivo)
+                s3.delete_object(Bucket=bucket_name, Key=ruta_directorio)
+                print(f"El archivo '{archivo}' se ha transferido correctamente.")
+                self.instancia.consola += f"El archivo '{archivo}' se ha transferido correctamente.\n"
+            except s3.exceptions.ClientError as e:
+                print(f"Ocurrió un error al copiar el archivo: {e}")
+                self.instancia.consola += f"Ocurrió un error al copiar el archivo: {e}\n"
+        else:
+            try:
+                # Verificar si la carpeta existe en S3
+                s3.head_object(Bucket=bucket_name, Key=ruta_directorio+'/')
+            except s3.exceptions.ClientError as e:
+                # Si ocurre un error, la carpeta no existe
+                if e.response['Error']['Code'] == '404':
+                    print(f"Error, la carpeta '{ruta_directorio}' no existe en S3.")
+                    self.instancia.consola += f"Error, la carpeta '{ruta_directorio}' no existe en S3.\n"
+                else:
+                    print(f"Error, ocurrió un error al verificar la carpeta: {e}")
+                    self.instancia.consola += f"Error, ocurrió un error al verificar la carpeta: {e}\n"
+                return
+
+            if not os.path.exists(ruta_destino):
+                try:
+                    os.makedirs(ruta_destino)
+                    # print(f"Se ha creado la ruta de destino '{ruta_destino}'.")
+                    # self.instancia.consola += f"Se ha creado la ruta de destino '{ruta_destino}'.\n"
+                except OSError as e:
+                    print(f"Error al crear la ruta de destino '{ruta_destino}': {e}")
+                    self.instancia.consola += f"Error al crear la ruta de destino '{ruta_destino}': {e}\n"
+                    return
+
+            # Descargar todos los objetos dentro de la carpeta
+            s3_resource = session.resource('s3')
+            bucket = s3_resource.Bucket(bucket_name)
+
+            for objeto in bucket.objects.filter(Prefix=ruta_directorio+'/'):
+                # Obtener la ruta completa del objeto
+                ruta_objeto = objeto.key
+
+                # Obtener la ruta relativa del objeto dentro de la carpeta
+                ruta_relativa = os.path.relpath(ruta_objeto, ruta_directorio)
+
+                # Construir la ruta de destino
+                ruta_destino_objeto = os.path.join(ruta_destino, ruta_relativa)
+
+                if objeto.key[-1] == '/':  # Es un directorio
+                    # Crear el directorio en la ruta de destino
+                    os.makedirs(ruta_destino_objeto, exist_ok=True)
+                else:  # Es un archivo
+                    # Descargar el archivo en la ruta de destino
+                    try:
+                        archivo_dividido = ruta_destino_objeto.split(".")
+                        nombre_sin_extension = archivo_dividido[0]
+                        extension = archivo_dividido[1]
+                        # Verificar si el archivo ya existe en la ruta de destino
+                        contador = 1
+                        while os.path.exists(ruta_destino_objeto):
+                            ruta_destino_objeto = f"{nombre_sin_extension}_copia{contador}.{extension}"
+                            contador += 1
+                        # bucket.download_file(objeto.key, ruta_destino_objeto)
+                        s3_resource.Object(bucket_name, ruta_objeto).download_file(ruta_destino_objeto)
+                        s3.delete_object(Bucket=bucket_name, Key=ruta_objeto)
+                    except s3.meta.client.exceptions.S3Exception as e:
+                        print(f"Error, ocurrió un error al copiar el archivo '{ruta_relativa}': {e}")
+                        self.instancia.consola += f"Error, ocurrió un error al copiar el archivo '{ruta_relativa}': {e}\n"
+            self.instancia.consola += f"La carpeta '{ruta_directorio}' se ha transferido correctamente.\n"
+            
+            objects_to_delete = []
+            result = s3.list_objects_v2(Bucket=bucket_name, Prefix=ruta_directorio)
+            for obj in result.get('Contents', []):
+                if obj['Key'] != carpeta:
+                    objects_to_delete.append({'Key': obj['Key']})
+
+            if objects_to_delete:
+                s3.delete_objects(Bucket=bucket_name, Delete={'Objects': objects_to_delete})
+                # print("El contenido de la carpeta se ha vaciado correctamente.")
+                # self.instancia.consola+="El contenido de la carpeta Archivos del bucket se ha vaciado correctamente.\n"
+            else:
+                print("La carpeta ya está vacía.")
+                self.instancia.consola+="La carpeta ya está vacía.\n"
     
     def Cloud(self):
         pass
